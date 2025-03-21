d37310967d3c883343737fb05621ff034be8ee66
joseandreaslie <jose.lie2208@gmail.com>
2025-03-21 11:37:51 +0700

    Init Push

---
+++ Bitemap/BitemapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitemapApp.swift
+//  Bitemap
+//
+//  Created by Jose Andreas Lie on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BitemapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitemapTests/BitemapTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitemapTests.swift
+//  BitemapTests
+//
+//  Created by Jose Andreas Lie on 21/03/25.
+//
+
+import Testing
+@testable import Bitemap
+
+struct BitemapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
